6c995878e8baa156353a55dcb761f4c15162e3d2
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2022-10-04 11:07:01 +0100

    feat: release year next to movie title

--- src/app/pages/movies/pages/movie/movie.component.ts
+++ src/app/pages/movies/pages/movie/movie.component.ts
@@ -16,6 +16,7 @@ import { IAppState } from 'src/app/state/interfaces/app-state.interface';
 import { Movie } from 'src/app/shared/models/movie.model';
 import { MoviesSelectors } from 'src/app/state/selectors/movies.selectors';
 import { MovieDetails } from 'src/app/shared/models/movie-details.model';
+import { DatesSelectors } from 'src/app/state/selectors/dates.selectors';
 
 @Component({
   selector: 'app-movie',
@@ -24,9 +25,14 @@ import { MovieDetails } from 'src/app/shared/models/movie-details.model';
 })
 export class MovieComponent implements OnInit, OnDestroy {
 
+  public dateFormat!: string;
+  public yearFormat!: string;
+
   public movieDetails!: MovieDetails | null;
 
   private movieDetails$: Observable<MovieDetails | null> = new Observable<Movie>();
+  private dateFormat$: Observable<string> = new Observable<string>();
+  private yearFormat$: Observable<string> = new Observable<string>();
 
   private destroy$ = new Subject<boolean>();
 
@@ -53,12 +59,23 @@ export class MovieComponent implements OnInit, OnDestroy {
 
   private initStoreSelectors(): void {
     this.movieDetails$ = this.store.select(MoviesSelectors.selectMovieDetails);
+    this.dateFormat$ = this.store.select(DatesSelectors.selectDateFormat);
+    this.yearFormat$ = this.store.select(DatesSelectors.selectYearFormat);
   }
 
   private initStoreSubscriptions(): void {
+
     this.movieDetails$
       .pipe(takeUntil(this.destroy$))
-      .subscribe((movie: Movie | null) => this.movieDetails = movie);
+      .subscribe((movieDetails: MovieDetails | null) => this.movieDetails = movieDetails);
+
+    this.dateFormat$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((dateFormat: string) => this.dateFormat = dateFormat);
+
+    this.yearFormat$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((yearFormat: string) => this.yearFormat = yearFormat);
   }
 
   private getMovieDetails(id: number | null): void {
